4f22ea6c2dac81bae02fad923e643947e553da09
Jose Antonio <josegrobles1997@gmail.com>
2020-05-13 11:13:37 +0200

    Add Docker files

---
+++ docker/init/startup.js
@@ -0,0 +1,2 @@
+db.createCollection('Books');
+db.Books.insertMany([{'Name':'Design Patterns','Price':54.93,'Category':'Computers','Author':'Ralph Johnson'}, {'Name':'Clean Code','Price':43.15,'Category':'Computers','Author':'Robert C. Martin'}]);
